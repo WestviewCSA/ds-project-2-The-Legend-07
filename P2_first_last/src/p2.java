59269fb78ce1a1cd936b67d1996d669180ffd7ef
1908001 <1908001@01-18109.pusd.dom>
2025-03-11 14:06:16 -0700

    Starting w/ diff classes

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -1,9 +1,48 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class p2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("p2");
+		
+		readMap("Test1");
+		
+	}
+	
+	
+	
+	public static void readMap(String filename) {
+		
+		File map = new File(filename);
+		try {
+			Scanner scan = new Scanner(map);
+			
+			int numRows = scan.nextInt();
+			int numCols = scan.nextInt();
+			int numRooms = scan.nextInt();
+			
+			int rowIndex = 0;
+			
+			//process the map!
+			while (scan.hasNextLine()) {
+				//grab a line (one row of the map)
+				String row = scan.nextLine();
+				if (row.length()>0) {
+					for (int i = 0; i < numCols && i < row.length(); i++) {
+						char el = row.charAt(i);
+						Tile obj = new Tile(rowIndex, i, el);
+					}
+				}
+				
+				
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
